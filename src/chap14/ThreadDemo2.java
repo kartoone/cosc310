58ecddbebb08930d2ee4a12c7acaef2c3662294b
Brian Toone <brtoone@samford.edu>
2025-04-15 14:45:28 -0500

    shared state example synchronization problem

--- src/chap14/ThreadDemo2.java
+++ src/chap14/ThreadDemo2.java
@@ -2,6 +2,110 @@ package chap14;
 
 import java.util.Random;
 
+public class ThreadDemo2 {
+    static byte[] nums = new byte[1_000_000]; 
+    static int shared_hit_counter = 0;
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("2");
+        initNums();
+
+        int TCOUNT=10;
+        Thread threads[] = new Thread[TCOUNT];
+        SearchWorker2 workers[] = new SearchWorker2[TCOUNT];
+        int slice = nums.length / threads.length;
+        // for (int i=0; i<TCOUNT; i++) {
+        //     threads[i] = new Thread(new HelloWorker(i));
+        //     threads[i].start();
+        // }
+        // for (int i = 0; i < threads.length; i++) {
+        //     workers[i] = new SumWorker(i*slice, i*slice+slice, nums);
+        //     threads[i] = new Thread(workers[i]);
+        //     threads[i].start();
+        // }
+        for (int i = 0; i < threads.length; i++) {
+            workers[i] = new SearchWorker2(i*slice, i*slice+slice, (byte) 3, nums);
+            threads[i] = new Thread(workers[i]);
+            threads[i].start();
+        }
+
+        // join is used to block until a thread finishes
+        // this loop will wait until continue all threads finishes
+        long grandtotal = 0L;
+        for (int i=0; i<TCOUNT; i++) {
+            // System.out.println("Waiting on thread: " + i);
+            threads[i].join();
+            grandtotal += workers[i].getHits();
+            System.out.println(workers[i].getHits() + " DONE waiting on thread: " + i);
+        }
+
+        // Since we are generated 21 different random numbers (-10 to 10), we can calculate the expected number of hits
+        // by multiplying the number of elements by the probability of hitting a number
+        // 1/21 = 0.047619047619047616
+        System.out.println("Exiting main: grand total hit count was: " + grandtotal);
+        System.out.println("Expected hit count: 1/21 = " + (1.0/21.0) + " * " + nums.length + " = " + Math.round(1.0/21.0 * nums.length));
+
+        // Now let's use our "shared state" worker without synchronization
+
+        SSWorker sworkers[] = new SSWorker[TCOUNT];
+        for (int i = 0; i < threads.length; i++) {
+            sworkers[i] = new SSWorker(i*slice, i*slice+slice, (byte) 3, nums);
+            threads[i] = new Thread(sworkers[i]);
+            threads[i].start();
+        }
+
+        // join is used to block until a thread finishes
+        // this loop will wait until continue all threads finishes
+        for (int i=0; i<TCOUNT; i++) {
+            threads[i].join();
+            System.out.println(" DONE waiting on thread: " + i);
+        }
+
+        // Since we are generated 21 different random numbers (-10 to 10), we can calculate the expected number of hits
+        // by multiplying the number of elements by the probability of hitting a number
+        // 1/21 = 0.047619047619047616
+        System.out.println("Exiting main: shared state hit count was: " + shared_hit_counter);
+        System.out.println("Independent workers hit count was: " + grandtotal);
+        System.out.println("Expected hit count: 1/21 = " + (1.0/21.0) + " * " + nums.length + " = " + Math.round(1.0/21.0 * nums.length));
+
+    }
+
+     public static void initNums() {
+        long start = System.currentTimeMillis();
+        Random rand = new Random();
+        for (int i=0; i<nums.length; i++) {
+            nums[i] = (byte) (rand.nextInt(21) - 10); // random number between -100,000 and 100,000
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken to initialize nums: " + (end - start) + " ms");        
+    }
+}
+
+class SSWorker implements Runnable {
+    protected int start; // inclusive
+    protected int end;   // exclusive
+    protected byte needle;
+    protected byte nums[]; // shared data we are accessing
+
+    SSWorker(int start, int end, byte needle, byte nums[]) {
+        this.start = start;
+        this.end = end;
+        this.needle = needle;
+        this.nums = nums;
+        System.out.println("Starting at: " + start + " and ending at: " + end);
+    }
+
+    @Override
+    public void run() {
+        for (int i = start; i < end; i++) {
+            if (nums[i]==needle)
+                ThreadDemo2.shared_hit_counter++;
+        }        
+    }
+
+}
+
+
+// NOTE: we already have a SearchWorker class inside our chap14 package, so we had to rename this to SearchWorker2
 class SearchWorker2 implements Runnable {
     protected int start; // inclusive
     protected int end;   // exclusive
@@ -75,52 +179,3 @@ class HelloWorker implements Runnable {
 
 }
 
-public class ThreadDemo2 {
-    static byte[] nums = new byte[100_000_000]; // 10 million elements
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("2");
-        initNums();
-
-        int TCOUNT=10;
-        Thread threads[] = new Thread[TCOUNT];
-        SearchWorker2 workers[] = new SearchWorker2[TCOUNT];
-        int slice = nums.length / threads.length;
-        // for (int i=0; i<TCOUNT; i++) {
-        //     threads[i] = new Thread(new HelloWorker(i));
-        //     threads[i].start();
-        // }
-        // for (int i = 0; i < threads.length; i++) {
-        //     workers[i] = new SumWorker(i*slice, i*slice+slice, nums);
-        //     threads[i] = new Thread(workers[i]);
-        //     threads[i].start();
-        // }
-        for (int i = 0; i < threads.length; i++) {
-            workers[i] = new SearchWorker2(i*slice, i*slice+slice, (byte) 3, nums);
-            threads[i] = new Thread(workers[i]);
-            threads[i].start();
-        }
-
-        // join is used to block until a thread finishes
-        // this loop will wait until continue all threads finishes
-        long grandtotal = 0L;
-        for (int i=0; i<TCOUNT; i++) {
-            // System.out.println("Waiting on thread: " + i);
-            threads[i].join();
-            grandtotal += workers[i].getHits();
-            System.out.println(workers[i].getHits() + " DONE waiting on thread: " + i);
-        }
-
-        System.out.println("Exiting main: grand total was: " + grandtotal);
-
-    }
-
-     public static void initNums() {
-        long start = System.currentTimeMillis();
-        Random rand = new Random();
-        for (int i=0; i<nums.length; i++) {
-            nums[i] = (byte) (rand.nextInt(21) - 10); // random number between -100,000 and 100,000
-        }
-        long end = System.currentTimeMillis();
-        System.out.println("Time taken to initialize nums: " + (end - start) + " ms");        
-    }
-}
