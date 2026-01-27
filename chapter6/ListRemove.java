5e1223579bb818d84febfaba05864aac0fbd0c90
Brian Toone <brtoone@samford.edu>
2026-01-27 12:53:18 -0600

    list remove partial implementation

---
+++ chapter6/ListRemove.java
@@ -0,0 +1,22 @@
+package chapter6;
+
+import java.util.ArrayList;
+
+public class ListRemove extends Target {
+
+    public ListRemove(int arr[], ArrayList<Integer> list, String name) {
+        super(arr, list, name);
+    }    
+
+    @Override
+    public int method(int[] indicesOrnums) {
+        int result = 0;
+        
+        // remove first 20,000 items from the list
+
+        return result;
+    }
+
+
+    
+}
