0d45706b2d96e9254a394a7c0d72a472c060d2aa
Brian Toone <brtoone@samford.edu>
2026-01-29 11:00:31 -0600

    clean-up

--- chapter6/ArrayVsArrayListLab.java
+++ chapter6/ArrayVsArrayListLab.java
@@ -8,27 +8,7 @@ import java.util.Random;
 
 public class ArrayVsArrayListLab {
 
-    
-
-    protected static int listRandomAccess(int indices[], ArrayList<Integer> list) {
-        int result = 0;
-        for (int i = 0; i < indices.length; i++) {
-            result += list.get(indices[i]);
-        }
-        return result;
-    }
-
     public static void main(String[] args) throws IOException {
-       /* long start = System.nanoTime();
-        System.out.println("hello, world");
-        long finish = System.nanoTime();
-        long elapsed = finish - start;
-        System.out.println("println: " + elapsed + "ns");
-        start = System.nanoTime();
-        arrayTest(30_000);
-        finish = System.nanoTime();
-        elapsed = finish - start;
-        System.out.println("arrayTest: " + elapsed + "ns"); */
         int arr[] = DataLoader.loadArray("numbers.txt");
         ArrayList<Integer> list = DataLoader.loadArrayList("numbers.txt");
         Random r = new Random();
@@ -39,19 +19,21 @@ public class ArrayVsArrayListLab {
 
         PrintWriter fileOut = new PrintWriter(new File("results.csv"));
         Target tests[] = new Target[8];
+        double testAverages[] = new double[8];
 
         tests[0] = new ArrayRandom(arr, list, "array,random_access");
         tests[1] = new ListRandom(arr, list, "arraylist, random_access");
         tests[2] = new ArrayAppend(arr, list, "array,append");
         
-        for (Target target : tests) {
+        for (int i=0; i<tests.length; i++) {
+            Target target = tests[i];
             if (target != null) {
-                target.runTests(indices);
+                testAverages[i] = target.runTests(indices);
                 target.writeResults(fileOut);
             }
         }
+        System.out.println(java.util.Arrays.toString(testAverages));
         
-
         fileOut.close();
         
     }
