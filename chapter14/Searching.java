75d8dc6347b86432214f4c883b92cd7c08c6441c
Brian Toone <brtoone@samford.edu>
2026-03-31 11:00:09 -0500

    binary search implemented for exact matches

--- chapter14/Searching.java
+++ chapter14/Searching.java
@@ -40,13 +40,120 @@ public class Searching {
         return null;
     }
 
-    public ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystact, int start, int end, long needle) {
-        return null;
+    // no need for index b/c we are only searching by timestamp (it's the only long field!)
+    public ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, int start, int end, long needle) {
+        if (start <= end) {
+            int mid = (start + end) / 2;
+            if (haystack.get(mid).getTimestamp() == needle) {
+                ArrayList<BikeDataRecord> hits = new ArrayList<>();
+                hits.add(haystack.get(mid));
+                // look left
+                int i = mid-1;
+                while (i>=0 && haystack.get(i).getTimestamp()==needle) {
+                    hits.add(haystack.get(i));
+                    i--;
+                }
+                // look right
+                i = mid+1;
+                while (i<haystack.size() && haystack.get(i).getTimestamp()==needle) {
+                    hits.add(haystack.get(i));
+                    i++;
+                }
+                return hits;
+            } else if (haystack.get(mid).getTimestamp() > needle) {
+                return binarySearch(haystack, start, mid-1, needle);
+            } else {
+                return binarySearch(haystack, mid+1, end, needle);
+            }
+        } else {
+            return new ArrayList<>(); // not found
+        }
     }
 
     public ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, long needle) {
         return binarySearch(haystack, 0, haystack.size()-1, needle);
     }
 
+    public ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, int start, int end, int needle, int index) {
+        if (start <= end) {
+            int mid = (start + end) / 2;
+            int val;
+            BikeDataRecord r = haystack.get(mid);
+            switch (index) {
+                case 2: val = r.getHeartrate(); break;
+                case 7: val = r.getPow(); break;
+                case 8: val = r.getCad(); break;
+                case 9: val = r.getDegC(); break;
+                default: val = 0;
+            }
+            if (val == needle) {
+                ArrayList<BikeDataRecord> hits = new ArrayList<>();
+                hits.add(r);
+                // look left
+                int i = mid-1;
+                if (i>=0) {
+                    r = haystack.get(i);
+                    switch (index) {
+                        case 2: val = r.getHeartrate(); break;
+                        case 7: val = r.getPow(); break;
+                        case 8: val = r.getCad(); break;
+                        case 9: val = r.getDegC(); break;
+                        default: val = 0;
+                    }
+                }
+                while (i>=0 && val==needle) {
+                    hits.add(r);
+                    i--;
+                    if (i>=0) {
+                        r = haystack.get(i);
+                        switch (index) {
+                            case 2: val = r.getHeartrate(); break;
+                            case 7: val = r.getPow(); break;
+                            case 8: val = r.getCad(); break;
+                            case 9: val = r.getDegC(); break;
+                            default: val = 0;
+                        }
+                    }
+                }
+                // look right
+                i = mid+1;
+                if (i<haystack.size()) {
+                    r = haystack.get(i);
+                    switch (index) {
+                        case 2: val = r.getHeartrate(); break;
+                        case 7: val = r.getPow(); break;
+                        case 8: val = r.getCad(); break;
+                        case 9: val = r.getDegC(); break;
+                        default: val = 0;
+                    }
+                }
+                 while (i<haystack.size() && val==needle) {
+                    hits.add(r);
+                    i++;
+                    if (i<haystack.size()) {
+                        r = haystack.get(i);
+                        switch (index) {
+                            case 2: val = r.getHeartrate(); break;
+                            case 7: val = r.getPow(); break;
+                            case 8: val = r.getCad(); break;
+                            case 9: val = r.getDegC(); break;
+                            default: val = 0;
+                        }
+                    }
+                }
+                return hits;
+            } else if (val > needle) {
+                return binarySearch(haystack, start, mid-1, needle, index);
+            } else {
+                return binarySearch(haystack, mid+1, end, needle, index);
+            }
+        } else {
+            return new ArrayList<>(); // not found
+        }
+    }
+
+    public ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, int needle, int index) {
+        return binarySearch(haystack, 0, haystack.size()-1, needle, index);
+    }
 
 }
