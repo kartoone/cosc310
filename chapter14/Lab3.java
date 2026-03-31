1719d27edf21e52bbbbea3903a1f5c5642fd9755
Brian Toone <brtoone@samford.edu>
2026-03-31 12:53:49 -0500

    binary search ranges for longs only plus examples of what you can do with this

--- chapter14/Lab3.java
+++ chapter14/Lab3.java
@@ -7,11 +7,45 @@ import json.*;
 
 public class Lab3 {
     public static void main(String[] args) throws Exception {
-        ArrayList<BikeDataRecord> records = BikeDataReader.parse("json/day2.json");
+        ArrayList<BikeDataRecord> records = new ArrayList<>();
+        ArrayList<BikeDataRecord> records1 = BikeDataReader.parse("json/day1.json");
+        ArrayList<BikeDataRecord> records2 = BikeDataReader.parse("json/day2.json");
+        ArrayList<BikeDataRecord> records3 = BikeDataReader.parse("json/day3.json");
+        ArrayList<BikeDataRecord> records4 = BikeDataReader.parse("json/day4.json");
+        records.addAll(records1);
+        records.addAll(records2);
+        records.addAll(records3);
+        records.addAll(records4);
         System.out.println(records.size());
-        System.out.println(records.get(20));
+        BikeDataRecord.sortCriteria = 0;
         List<BikeDataRecord> sortedRecords = Sorting.mergeSort(records);
-        System.out.println(records.get(0));
+        System.out.println(sortedRecords.get(0));
+        System.out.println(sortedRecords.get(1));
+        System.out.println(sortedRecords.get(2));
+        System.out.println("...");
+        int count = 0;
+        for (BikeDataRecord bikeDataRecord : sortedRecords) {
+            if (bikeDataRecord.getHeartrate()>0) {
+                System.out.println(bikeDataRecord);
+                count++;
+            }
+            if (count>10)
+                break;
+        }
+
+        ArrayList<BikeDataRecord> lunchday1  = Searching.binarySearch((ArrayList<BikeDataRecord>)sortedRecords, 1141920754L, 1141923454L);
+        System.out.println(lunchday1.size());
+        int hrtotal = lunchday1.stream().map(BikeDataRecord::getHeartrate).reduce(0, Integer::sum);
+        int hravg = hrtotal/lunchday1.size();
+        System.out.println(hravg);
+
+        ArrayList<BikeDataRecord> lunchday3  = Searching.binarySearch((ArrayList<BikeDataRecord>)sortedRecords, 1141920754L+72800, 1141923454L+72800);
+        System.out.println(lunchday3.size());
+        int hrtotal3 = lunchday3.stream().map(BikeDataRecord::getHeartrate).reduce(0, Integer::sum);
+        int hravg3 = hrtotal3/lunchday3.size();
+        System.out.println(hravg3);
+        int[][] radarArrayTest = new int[][] { new int[] {10, 3}, new int[] {20, 6, 7, 8, 9}, new int[] { 50, 10} };
+        System.out.println(radarArrayTest[1].length);
 
 
     }
--- chapter14/Searching.java
+++ chapter14/Searching.java
@@ -7,7 +7,7 @@ import json.BikeDataRecord;
 
 public class Searching {
     
-    public ArrayList<BikeDataRecord> linearSearch(ArrayList<BikeDataRecord> haystack, long needle) {
+    public static ArrayList<BikeDataRecord> linearSearch(ArrayList<BikeDataRecord> haystack, long needle) {
         ArrayList<BikeDataRecord> hits = new ArrayList<>();
         Iterator<BikeDataRecord> it = haystack.iterator();
         while(it.hasNext()) {
@@ -20,7 +20,7 @@ public class Searching {
     }
 
     // only valid indices are 2, 7, 8, 9
-    public ArrayList<BikeDataRecord> linearSearch(int val, int index) {
+    public static ArrayList<BikeDataRecord> linearSearch(int val, int index) {
         ArrayList<BikeDataRecord> hits = new ArrayList<>();
         Iterator<BikeDataRecord> it = hits.iterator();
         while(it.hasNext()) {
@@ -35,13 +35,13 @@ public class Searching {
         return hits;
     }
     
-    public ArrayList<BikeDataRecord> linearSearch(float val, int index) {
+    public static ArrayList<BikeDataRecord> linearSearch(float val, int index) {
         // to do: search by the other fields that are floats
         return null;
     }
 
     // no need for index b/c we are only searching by timestamp (it's the only long field!)
-    public ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, int start, int end, long needle) {
+    public static ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, int start, int end, long needle) {
         if (start <= end) {
             int mid = (start + end) / 2;
             if (haystack.get(mid).getTimestamp() == needle) {
@@ -70,11 +70,45 @@ public class Searching {
         }
     }
 
-    public ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, long needle) {
+    // inclusive on BOTH ends because we are not talking about positions
+    public static ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, int start, int end, long min_needle, long max_needle) {
+        if (start <= end) {
+            int mid = (start + end) / 2;
+            if (haystack.get(mid).getTimestamp() >= min_needle && haystack.get(mid).getTimestamp() <= max_needle) {
+                ArrayList<BikeDataRecord> hits = new ArrayList<>();
+                hits.add(haystack.get(mid));
+                // look left
+                int i = mid-1;
+                while (i>=0 && haystack.get(i).getTimestamp()>=min_needle && haystack.get(i).getTimestamp()<=max_needle) {
+                    hits.add(haystack.get(i));
+                    i--;
+                }
+                // look right
+                i = mid+1;
+                while (i<haystack.size() && haystack.get(i).getTimestamp()>=min_needle && haystack.get(i).getTimestamp()<=max_needle) {
+                    hits.add(haystack.get(i));
+                    i++;
+                }
+                return hits;
+            } else if (haystack.get(mid).getTimestamp() > max_needle) {
+                return binarySearch(haystack, start, mid-1, min_needle, max_needle);
+            } else {
+                return binarySearch(haystack, mid+1, end, min_needle, max_needle);
+            }
+        } else {
+            return new ArrayList<>(); // not found
+        }
+    }
+
+    public static ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, long needle) {
         return binarySearch(haystack, 0, haystack.size()-1, needle);
     }
 
-    public ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, int start, int end, int needle, int index) {
+    public static ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, long min_needle, long max_needle) {
+        return binarySearch(haystack, 0, haystack.size()-1, min_needle, max_needle);
+    }
+
+    public static ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, int start, int end, int needle, int index) {
         if (start <= end) {
             int mid = (start + end) / 2;
             int val;
@@ -152,7 +186,7 @@ public class Searching {
         }
     }
 
-    public ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, int needle, int index) {
+    public static ArrayList<BikeDataRecord> binarySearch(ArrayList<BikeDataRecord> haystack, int needle, int index) {
         return binarySearch(haystack, 0, haystack.size()-1, needle, index);
     }
 
